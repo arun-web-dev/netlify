bda28fab10096027fd7785f6d7df4fa2f1458a7b
arun-web-dev <miuiandrew@gmail.com>
2021-12-18 10:09:36 +0530

    navigation menu active class updated

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
 "use sctrict";
 const hamburger = document.querySelector(".hamburger");
 const navlinks = document.querySelector(".nav-links-menu");
+const btns = document.getElementsByClassName("menu_button");
 
 hamburger.addEventListener("click", () => {
   navlinks.classList.toggle("nav-active");
   hamburger.classList.toggle("change");
 });
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
